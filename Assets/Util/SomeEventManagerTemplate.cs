561349aaf172d80b408de60a6deafa70abe1daef
dim <web.best.master@gmail.com>
2022-08-09 23:36:48 +0200

    upd

---
+++ Assets/Util/SomeEventManagerTemplate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public static class SomeEventManagerTemplate
+{
+    public delegate void SomeAction(int argument);
+
+    public static event SomeAction gameSomeAction;
+
+    public static void TriggerSomeEvent(int argument)
+    {
+        gameSomeAction?.Invoke(argument);
+    }
+}
+
+/*
+public ModelValue(ValueType defaultValue)
+{
+    SomeEventManagerTemplate.gameSomeAction += TestAction;
+    // use `SomeEventManagerTemplate.gameSomeAction -= TestAction;`
+    // to unsubscrube
+    
+    SomeEventManagerTemplate.TriggerSomeEvent(11);
+}
+
+void TestAction(int value)
+{
+    Debug.Log(value);
+}
+*/
