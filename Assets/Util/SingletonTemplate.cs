4a6f0d578d90509279e7d21baf1e07c93eb62d9c
Dmitry Turovtsov <dmitry.turovtsov@consultant.volvo.com>
2022-08-11 11:26:05 +0200

    upd

---
+++ Assets/Util/SingletonTemplate.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Singleton : MonoBehaviour
+{
+    public static Singleton Instance { get; private set; }
+
+    private void Awake()
+    {
+        // If there is an instance, and it's not me, delete myself.
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+}
+
+/*
+    Singleton.Instance.SomeMethod(someArgument);
+*/
